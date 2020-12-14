f88c99ce637d8483ae5845154aea35499fd6fadc
Martin VanWinkle III <mvanwinkle@ias.edu>
2020-12-14 07:18:36 -0500

    progress

--- src/bin/git_files_changed_since_last_tag.sh
+++ src/bin/git_files_changed_since_last_tag.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+tags=$( git tag )
+
+if [[ -z "$tags" ]]
+then
+	first_checkin=$( git log --oneline | tail -1 | awk '{print $1}' )
+	git diff --name-only HEAD $first_checkin
+	exit $?
+fi
 git diff --name-only HEAD $(git describe --tags --abbrev=0)
