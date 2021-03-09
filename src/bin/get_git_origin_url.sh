0f424932a392c60a369a8529019a9679bc6f4bf6
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-03-09 11:24:04 -0500

    progress

--- src/bin/get_git_origin_url.sh
+++ src/bin/get_git_origin_url.sh
@@ -2,11 +2,19 @@
 
 # In a project depot, cd into all of the directories and output
 # the remote.origin.url
-dir="${1:-d}"
+dir="${1:-.}"
 start_dir=`pwd`
 # echo "Dir: $dir"
 cd "$dir"
 result=$( git config --get remote.origin.url )
+status=0
+
+if [ -z "$(git status --porcelain)" ]
+then
+	:
+else
+	status=1
+fi
 tab=$( echo -e '\t' )
-echo "${dir}${tab}${result}"
+echo "${dir}${tab}${result}${tab}${status}"
 cd "$start_dir"
