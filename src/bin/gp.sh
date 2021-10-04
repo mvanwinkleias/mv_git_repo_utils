4699b4bb5159014502e2bb03df8365a55e0192ee
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-10-04 11:51:22 -0400

    verbose mode, silent without issues

--- src/bin/gp.sh
+++ src/bin/gp.sh
@@ -1,10 +1,59 @@
 #!/bin/bash
 
+verbose=0
+
+git_pull_command=( \
+	git pull \
+)
+
+function verbose_output
+{
+	if [[ "$verbose" == "1" ]]
+	then
+		echo "$@"
+	fi
+}
+
+function usage
+{
+	echo "-v : enable verbose"
+}
+while getopts "v" o; do
+    case "${o}" in
+        v)
+            verbose=1
+            ;;
+        *)
+            usage
+			exit 1
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
 
 old_cwd=$( pwd )
 path="$1"
 
-cd $path
-echo "----- Pulling: $path"
-git pull
-cd "$old_pwd"
+cd_back=0
+if [[ ! -z "$path" ]]
+then
+	cd $path
+	cd_back=1
+else
+	path="."
+fi
+
+verbose_output "----- Pulling: $path"
+
+if [[ "$verbose" == 1 ]]
+then
+	git pull
+else
+	git pull > /dev/null
+fi
+
+if [[ "$cd_back" == 1 ]]
+then
+	cd "$old_cwd"
+fi
