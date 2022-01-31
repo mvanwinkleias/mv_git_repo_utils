8f122c43c9445733e0fdf704993eb157a5fe6b26
Martin VanWinkle III <mvanwinkle@ias.edu>
2022-01-30 21:31:48 -0500

    back to where we started.

--- src/bin/git-remove-local-only-branches.sh
+++ src/bin/git-remove-local-only-branches.sh
@@ -18,4 +18,16 @@ function git_get_all_remote_branches
 }
 # grep -Ev -f <(git branch -r | awk '{print $1}') <(git branch -vv | grep origin | awk '{print $1'})
 
-git remote prune origin
+# git remote prune origin
+
+# The sed command below might be overkill.  Apparently you can't
+# create branch names with spaces in them, but what's going on here is
+# it removes everything after the commit hash.
+# | sed -E 's/^[ \t]*//;s/\S+\s\[.*$//' \
+git branch -vv \
+| grep 'gone]' \
+# | xargs -n1 git branch -d
+
+# It's looking like because git allows for branch names like gone]
+# that we might have to find a better way of doing things.
+
